29c691b5a0e23dacc2859da273969d145e404a31
VillanValentine <villanvalentine@gmail.com>
2022-06-06 12:10:16 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,57 +7,18 @@
 * Description: random number in n +ve/-ve
 * Return: Always 0 (success)
 */
-
-
-
 int main(void)
-
-
-
 {
-
-
-
 int firstSLetter, firstCletter, n;
 
-
-
 for (firstSLetter = 97, n = 1; n < 27; firstSLetter++, n++)
-
-
-
 {
-
-
-
 putchar(firstSLetter);
-
-
-
 }
-
-
-
 for (firstCletter = 65, n = 1; n < 27; firstCletter++, n++)
-
-
-
 {
-
-
-
 putchar(firstCletter);
-
-
-
 }
-
-
-
 putchar(10);
-
-
-
 return (0);
-
 }

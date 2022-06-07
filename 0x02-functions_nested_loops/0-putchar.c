386a299b5733f43c6a887b781d03cf7258614da0
VillanValentine <villanvalentine@gmail.com>
2022-06-07 11:25:27 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <main.h>
+#include "main.h"
 /**
-* main - Print "_putchar"
-* Return: Always 0 (Success)
+* main - prints Putchar
+*
+* Description: prints _putchar
+*
+* Return: 0 (Success)
 */
 int main(void)
 {
-int i = 0;
-char name_of_school[] = "_putchar"
-while (i < 9)
-{
-_putchar(name_of_school[i]);
-i++;
-}
+_putchar('_');
+
+_putchar('p');
+
+_putchar('u');
+
+_putchar('t');
+
+_putchar('c');
+
+_putchar('h');
+
+_putchar('a');
+
+_putchar('r');
 _putchar('\n');
 return (0);
 }

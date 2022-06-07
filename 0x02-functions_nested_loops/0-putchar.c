7ef20e2e9ab449960311db0ef4a0abd3af001883
VillanValentine <villanvalentine@gmail.com>
2022-06-07 09:07:50 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* main - Print "_putchar"
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i = 0;
+char name_of_school[] = "_putchar"
+while (i < 9)
+{
+_putchar(name_of_school[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}

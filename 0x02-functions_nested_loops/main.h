51913fe341a836db7443b3000514b85860bbf7da
VillanValentine <villanvalentine@gmail.com>
2022-06-08 03:17:38 -0700

    main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,28 @@
+nclude "../main.h"
+
+
+
+/**
+*
+* main - Entry point
+*
+*
+*
+* Description: check the code for ALX School students
+*
+*
+*
+* Return: Always 0 (Success)
+*
+*/
+
+
+
+int main(void)
+
+{
+
+print_alphabet();
+
+return (0);
+}

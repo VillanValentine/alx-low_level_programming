546f562ca4d6d8aedbcd3ad1d52e01e781886e96
VillanValentine <villanvalentine@gmail.com>
2022-06-13 11:28:22 -0700

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,9 +4,11 @@
 /**
  * swap_int - swaps the values of two integers
  * using two input parameters
- *
+ * @a: input parameter 1
+ * @b: input parameter 2
  * Return: Always 0
  */
+
 void swap_int(int *a, int *b)
 {
 	int temp;

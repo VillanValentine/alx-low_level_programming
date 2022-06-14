53605bd407df9abefdaec1b4dfbb6797d08f3468
VillanValentine <villanvalentine@gmail.com>
2022-06-14 08:34:34 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,11 +10,11 @@ void puts_half(char *str)
 {
 	int l, n;
 
-	for (l = 0; str[l] != '\0';++1);
+	for (l = 0; str[l] != '\0'; ++1);
 			
 	if (l % 2 == 0)
 	{
-		for (n = l / 2; str[n] != '\0';++n)
+		for (n = l / 2; str[n] != '\0'; ++n)
 				_putchar(str[n]);
 	}
 	else
